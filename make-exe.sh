f69b1abf831b7e490829ed2f8e6e90b5421f7bf3
Ahsan Zeb <ahsan.zeb@hotmail.com>
2016-12-20 19:39:12 +0000

    commented the makexe file

--- make-exe.sh
+++ make-exe.sh
@@ -1,12 +1,22 @@
 
+# python version you want to use:
+pythonpath='#!/usr/local/bin/python3.5';
+# executable name:
+exe='polaritons'
+
+# make a zip of src, add the pythonpath on top, and make it executable 
 cd src
 zip -r ../x.zip *
 cd ..
-
-
-echo '#!/usr/local/bin/python3.5' | cat - x.zip > polaritons
-chmod +x polaritons
+echo $pythonpath | cat - x.zip > $exe
+chmod +x $exe
 rm x.zip
+
+### if want to keep the executable to bin:
+# bin='/Users/panda/bin'
+# mv $exe $bin
+### if need to add the path of bin to PATH:
+# echo "PATH=$bin:"'$PATH': >> ~/.bashrc
 
 
 
