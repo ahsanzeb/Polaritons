7ba8ea710534c4ce1fa40cc56c2ae1cff431308c
Ahsan Zeb <ahsan.zeb@hotmail.com>
2016-12-23 22:10:03 +0000

    due to extra basis, mx>m case: enabled conditional dm calc. new functions for Nphoton=1 . old functions with m ==> mx in a few places for Nexciton=1. not tested yet.

--- src/densitymatrices.py
+++ src/densitymatrices.py
@@ -48,10 +48,10 @@ def getrho0(jchunk):
 	for jj in range(jchunk[0],jchunk[1]):
 		m1 = o.Norm2l[jj];
 		for mj1 in range(0,m+1):
+			i1 = o.map21[jj,mj1];
+			m11=o.Norm1l[i1];
 			for mj2 in range(mj1,m+1): # upper triangular of rho0 only
-				i1 = o.map21[jj,mj1];
 				i2 = o.map21[jj,mj2];
-				m11=o.Norm1l[i1];
 				m12=o.Norm1l[i2];
 				xx = np.sqrt(m11*m12);
 				for il, evalu, evec in o.eigvv:
@@ -61,16 +61,106 @@ def getrho0(jchunk):
 					dm0l[il,mj1,mj2] += xij;
 	return dm0l
 #****************************************************************
+
+
+#--------------------------------------------
+# btw extra and extra, type 1:
+#--------------------------------------------
+# diag in set jj: 
+# rho[mj1,mj2]; mj1,mj2 special site occupation
+def getrho0P2ss(jchunk):
+	dm0l = np.zeros((nlmax,mx+1,mx+1));
+	nm = n1fsym - (m+1)*n2;
+	for jj in range(jchunk[0],jchunk[1]):
+		nmj = nm + jj;
+		for mj1 in range(m+1,mx+1):
+			i1 = o.map21[jj,mj1] + nmj;
+			for mj2 in range(mj1,mx+1): # upper triangular of rho0 only
+				i2 = o.map21[jj,mj2] + nmj;
+				for il, evalu, evec in o.eigvv:
+					ci=np.conjugate(evec[i1][0]);
+					cj=evec[i2][0];
+					dm0l[il,mj1,mj2] += ci*cj;
+	return dm0l
+#---------------------------
+# btw extra and extra, type 2:
+#----------------------------
+# diag in special site mj: 
+# rho[mk1,mk2]; mk1,mk2 in set jj
+# that reduce to the same sym set kk (for n-2 sites).
+def getrho0P2ss(kchunk):
+	dm0l = np.zeros((nlmax,mx+1,mx+1));
+	nm = n1fsym - (m+1)*n2;
+	for kk in range(kchunk[0],kchunk[1]):
+		Pkkn = o.Norm3l[kk] * (n-1)/n;
+		for mk1 in range(0,m+1):
+			jj1 = o.map32[kk,mk1];
+			Pj1 = o.Norm2l[jj1];
+			nmj1 = nm + jj1;	
+			for mk2 in range(mk1,m+1): # upper triangular of rho0 only
+				jj2 = o.map32[kk,mk2];
+				Pj2 = o.Norm2l[jj2];
+				nmj2 = nm + jj2;	
+				xij = Pkkn* np.sqrt(Pj1*Pj2);
+				for mj in range(m+1,mx+1):
+					i1 = mj + nmj1;
+					i2 = mj + nmj2;
+					for il, evalu, evec in o.eigvv:
+						ci=np.conjugate(evec[i1][0]);
+						cj=evec[i2][0];
+						dm0l[il,mk1,mk2] += ci*cj*xij;
+	return dm0l
+#-------------------------
+# n==2 version:
+def getrho0P2ssn2(mj1):
+		dm0l = np.zeros((nlmax,mx+1,mx+1));
+		nm = n1fsym - (m+1)*n2;
+		xij0=0.5;#m1*(n-1)/(n*xx); m1=1,xx=1
+		#for mj1 in range(0,m+1):
+		nmj1 = nm + mj1;	
+		for mj2 in range(mj1,m+1):
+			nmj2 = nm + mj2;	
+			for mj in range(m+1,mx+1):
+				i1 = mj*n2 + nmj1;
+				i2 = mj*n2 + nmj2;
+				for il, evalu, evec in o.eigvv:
+					ci=np.conjugate(evec[i1][0]);
+					cj=evec[i2][0];
+					dm0l[il,mk1,mk2] += ci*cj*xij0;
+	return dm0l
+#--------------------------------------------
+# btw fsym and extra:
+#--------------------------------------------
+# special site mj > m so contrib to only rho[mj1,mj],
+# with mj1 in fsym set i that reduce to the same sym set
+# jj as for extra state's sym set part.
+def getrho0P2os(jchunk):
+	dm0l = np.zeros((nlmax,mx+1,mx+1));
+	nm = n1fsym - (m+1)*n2;
+	for jj in range(jchunk[0],jchunk[1]):
+		Pjj = o.Norm2l[jj];
+		nmj = nm + jj;
+		for mj1 in range(0,m+1):
+			i1 = o.map21[jj,mj1];
+			Pi1 = o.Norm1l[i1];
+			for mj2 in range(m+1,mx+1): # upper triangular of rho0 only
+				i2 = nmj + mj2*n2;
+				xij = np.sqrt(n*Pjj/Pi1);
+				for il, evalu, evec in o.eigvv:
+					ci=np.conjugate(evec[i1][0]);
+					cj=evec[i2][0];
+					dm0l[il,mj1,mj2] += ci*cj*xij;
+	return dm0l
+
+#****************************************************************
 # calc of dm1
 #****************************************************************
 def getrho1(jchunk):
-	# dm1l = [[[0.0]*(m+1) for x in range(m+1)] for il in range(nlmax)];
-	# dm1l = np.ndarray(dm1l);
-	dm1l = np.zeros((nlmax,m+1,m+1));
+	dm1l = np.zeros((nlmax,mx+1,mx+1));
 	for i in range(jchunk[0],jchunk[1]):
-		for mi in range(0,m+1):
+		for mi in range(0,mx+1):
 			ii = mi*n2 + i # index of basis state in this block
-			for mj in range(mi,m+1): # only upper triangular
+			for mj in range(mi,mx+1): # only upper triangular
 				jj = mj*n2 + i  # only for j = i terms contrib
 				for il, evalu, evec in o.eigvv:
 					ci=np.conjugate(evec[n1+ii][0]);
@@ -82,21 +172,19 @@ def getrho1(jchunk):
 # calc of dm2
 #****************************************************************
 def getrho2(kchunk):
-	#dm2l = [[[0.0]*(m+1) for x in range(m+1)] for il in range(nlmax)];
-	#dm2l = np.ndarray(dm2l);
 	dm2l = np.zeros((nlmax,m+1,m+1));
 	for kk in range(kchunk[0],kchunk[1]):
-		m1 = o.Norm3l[kk];
+		m1n = o.Norm3l[kk] * (n-1)/n;
 		for mj1 in range(0,m+1):
+			i1 = o.map32[kk,mj1];
+			m11=o.Norm2l[i1];
 			for mj2 in range(mj1,m+1): # upper triangular of rho0 only
-				i1 = o.map32[kk,mj1];
 				i2 = o.map32[kk,mj2];
-				m11=o.Norm2l[i1];
 				m12=o.Norm2l[i2];
 				xx = np.sqrt(m11*m12);
-				xij0=m1*(n-1)/(n*xx);
+				xij0=m1n/xx;
 				for il, evalu, evec in o.eigvv:
-					for m2 in range(0,m+1):
+					for m2 in range(mx+1):
 						ii1=m2*n2 + i1; # diag in m2 contrib only
 						ii2=m2*n2 + i2;
 						ci=np.conjugate(evec[n1+ii1][0]);
@@ -108,15 +196,13 @@ def getrho2(kchunk):
 # dm2 for n=2 case
 def getrho2n2():
 	dm2l = np.zeros((nlmax,m+1,m+1));
+	xij0=0.5;#m1*(n-1)/(n*xx); m1=1,xx=1
 	for mj1 in range(0,m+1):
-			i1 = mj1;
 			for mj2 in range(mj1,m+1): # upper triangular of rho0 only
-				i2 = mj2;
-				xij0=0.5;#m1*(n-1)/(n*xx); m1=1,xx=1
 				for il, evalu, evec in o.eigvv:
-					for m2 in range(0,m+1):
-						ii1=m2*n2 + i1; # diag in m2 contrib only
-						ii2=m2*n2 + i2; 
+					for m2 in range(mx+1):
+						ii1=m2*n2 + mj1; # diag in m2 contrib only
+						ii2=m2*n2 + mj2; 
 						ci=np.conjugate(evec[n1+ii1][0]);
 						cj=evec[n1+ii2][0];
 						xij = xij0*ci*cj;
@@ -180,9 +266,25 @@ def cdms():
 	# calc of dm0
 	#*******************************
 	print('       calculating dm0 ... ')
+	# original fully sym basis, now first n1fsym basis in photon block
 	dm0s = pool.map(getrho0,listn2)  # note list2
-	dm0 = sum(dm0s,0) # sum along which dim, 3? or 0?
+	dm0 = sum(dm0s,0) # sum along 0 ==> pool map
+	# due to extra basis ---------------
+	# extra-extra: type 1
+	dm0s = pool.map(getrho0P2ss,listn2);
+	dm0 += sum(dm0s,0);
+	# extra-extra: type 2
+	dm0s = pool.map(getrho0P2os,listn2)
+	dm0 += sum(dm0s,0);
+	# fsym-extra:
+	if n==2:
+		dm0s = pool.map(getrho0P2ssn2,range(0,m+1))
+		dm0 += sum(dm0s,0);
+	elif n>2:
+		dm0s = pool.map(getrho0P2ss,listn3)
+		dm0 += sum(dm0s,0);
 	del dm0s # free memory
+	# -----------------------------------
 	writedms(dm0,param,lambin0,fout0);
 	#*********************************
 	# calc of dm0
